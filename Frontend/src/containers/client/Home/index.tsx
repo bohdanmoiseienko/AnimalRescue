f139fe9a65ac414c2d271598deaa2e09f58e446d
bohdan <bohdan.moiseienko@globallogic.com>
2019-12-04 12:05:08 +0200

    added i18n

--- Frontend/src/containers/client/Home/index.tsx
+++ Frontend/src/containers/client/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {RouteComponentProps} from "react-router";
 import {Link} from "react-router-dom";
+import {TI18n} from '../../../i18n';
 
 interface IPropTypes extends RouteComponentProps<any>{
 
@@ -11,7 +12,7 @@ const Home: React.FC<IPropTypes> = (props: IPropTypes) => {
     return (
         <div>
             Home
-            <Link to="/admin">Admin</Link>
+            <Link to="/admin"><TI18n keyStr="admin" default="Admin"/></Link>
         </div>
     )
 };
